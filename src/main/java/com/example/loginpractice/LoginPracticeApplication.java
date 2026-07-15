bd1225b78bdc013aa4ada52b138e7a92540070eb
mxnssuu <paul990129@gmail.com>
2026-07-15 13:38:06 +0900

    feat: implement a subscription api
    auto create role at app startup (trainee/manager/super_admin)
    - h2 db setup and console activation
    - add api test files

--- src/main/java/com/example/loginpractice/LoginPracticeApplication.java
+++ src/main/java/com/example/loginpractice/LoginPracticeApplication.java
@@ -1,7 +1,11 @@
 package com.example.loginpractice;
 
+import com.example.loginpractice.entity.Role;
+import com.example.loginpractice.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LoginPracticeApplication {
@@ -10,4 +14,15 @@ public class LoginPracticeApplication {
         SpringApplication.run(LoginPracticeApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner initRoles(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.count() == 0) {
+                roleRepository.save(new Role(null, "TRAINEE", "교육생"));
+                roleRepository.save(new Role(null, "MANAGER", "매니저"));
+                roleRepository.save(new Role(null, "SUPER_ADMIN", "슈퍼어드민"));
+                System.out.println(">>> 역할 3개 생성 완료");
+            }
+        };
+    }
 }
